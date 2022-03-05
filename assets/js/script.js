c39493119b5384c90fa3bf86432cf2d8f51f081d
John-Pena <jmp1495@gmail.com>
2022-03-05 15:28:38 -0600

    broken code, won't register answer selection, wont' cycle through questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,71 +10,79 @@ var answerDText = document.getElementById('btn-d')
 var questionCounter = 0;
 questionCounter++;
 
-var quizInfo = function () {
+var quizInfo = function() {
     infoEl.classList.add('hidden');
     questionContainer.classList.remove('hidden');
     startQuiz();
 }
 
-var startQuiz = function () {
+var startQuiz = function() {
 
-    questionText.textContent = questions[0].question;
+   questionText.textContent = questions[0].question;
 
-    answerAText.textContent = questions[0].answers[0].text;
-    answerBText.textContent = questions[0].answers[1].text;
-    answerCText.textContent = questions[0].answers[2].text;
-    answerDText.textContent = questions[0].answers[3].text;
+   answerAText.textContent = questions[0].answers[0].text;
+   answerBText.textContent = questions[0].answers[1].text;
+   answerCText.textContent = questions[0].answers[2].text;
+   answerDText.textContent = questions[0].answers[3].text;
 }
 
+var userAnswer = function() {
+    if (this.value !== questions[0].answers.correct) {
+        // penalize time
+        // if time less than 0, end quiz
+        // alert for wrong or correct
 
+        
+    }
+    // else would be correct
+}
 
 startButton.addEventListener('click', quizInfo);
-
 
 var questions = [
     {
         question: 'Arrays in JavaScript can be used to store ____?',
         answers: [
-            { text: 'numbers and strings', correct: false },
-            { text: 'booleans', correct: false },
-            { text: 'other arrays', correct: false },
-            { text: 'All of the above', correct: true }
+            {text: 'numbers and strings', correct: false},
+            {text: 'booleans', correct: false},
+            {text: 'other arrays', correct: false},
+            {text: 'All of the above', correct: true}
         ]
     },
     {
         question: 'A string must be closed with a ____ when assigning a variable',
         answers: [
-            { text: 'curly bracket', correct: false },
-            { text: 'paranthesis', correct: false },
-            { text: 'quotes', correct: true },
-            { text: 'sqaure brackets', correct: false }
+            {text: 'curly bracket', correct: false},
+            {text: 'paranthesis', correct: false},
+            {text: 'quotes', correct: true},
+            {text: 'sqaure brackets', correct: false}
         ]
     },
     {
         question: 'The condition of an if/else statement must be closed with ____',
         answers: [
-            { text: 'commas', correct: false },
-            { text: 'paranthesis', correct: true },
-            { text: 'curly brackets', correct: false },
-            { text: 'quotes', correct: false }
+            {text: 'commas', correct: false},
+            {text: 'paranthesis', correct: true},
+            {text: 'curly brackets', correct: false},
+            {text: 'quotes', correct: false}
         ]
     },
     {
         question: 'How would you terminate a while loop',
         answers: [
-            { text: 'break', correct: true },
-            { text: 'if', correct: false },
-            { text: 'switch', correct: true },
-            { text: 'function', correct: false }
+            {text: 'break', correct: true},
+            {text: 'if', correct: false},
+            {text: 'switch', correct: true},
+            {text: 'function', correct: false}
         ]
     },
     {
         question: 'Commonly stored data types DO NOT include',
         answers: [
-            { text: 'strings', correct: false },
-            { text: 'booleans', correct: false },
-            { text: 'alert', correct: true },
-            { text: 'numbers', correct: false }
+            {text: 'strings', correct: false},
+            {text: 'booleans', correct: false},
+            {text: 'alert', correct: true},
+            {text: 'numbers', correct: false}
         ]
     },
 ]
