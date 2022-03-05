69e10d735018099541150b40b247e66e2b0a1474
John-Pena <jmp1495@gmail.com>
2022-03-04 20:18:46 -0600

    index page initial setup created

---
+++ assets/js/script.js
@@ -0,0 +1,35 @@
+// You have a time limit for answering these 5 questions
+// Once you have selected your answer, you cannot reselect
+// You will earn points based off your correct answers
+// Getting a question wrong will deduct 10 seconds off your total time
+// When timer has run out, the quiz qill be over
+
+// Arrays in JavaScript can be used to store ____?
+// numbers and strings false
+// booleans false
+// other arrays false
+// All of the above true
+
+// A string must be closed with a ____ when assigning a variable
+// curly bracket false
+// paranthesis false
+// quotes true
+// sqaure brackets false
+
+// The condition of an if/else statement must be closed with ____
+// commas false
+// parantheses true
+// curly brackets false
+// quotes false
+
+// How would you terminate a while loop
+// break true
+// return false
+// switch false
+// function false
+
+// commonly stored data types DO NOT include
+// Strings false
+// Boolean false
+// alert true
+// numbers false
